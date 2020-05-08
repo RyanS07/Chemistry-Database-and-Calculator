d04b937d032bfc000b86115311091373d959864a
rseto07 <63327161+rseto07@users.noreply.github.com>
2020-05-08 14:15:22 -0400

    Complete revamp

--- Test.java
+++ Test.java
@@ -1,28 +1,17 @@
 import java.util.Scanner;
-import classes.SigFig;
+import classes.calculators.Cross;
+import classes.calculators.SigFig;
 
 public class Test {
     public static void main(String[] args) {
-        // for(int i = 0; i < 10; i++) {
-        //     System.out.println("Test " + (i+1) + ":");
-        //     double a = Math.random() * 100;
-        //     double b = Math.random() * 100;
-        //     int sf1 = (int) (Math.random() * 5);
-        //     int sf2 = (int) (Math.random() * 5);
-        //     String x = String.format("%." + sf1 + "g", a);
-        //     String y = String.format("%." + sf2 + "g", b);
-        //     System.out.println("X: " + x);
-        //     System.out.println("Y: " + y);
-        //     System.out.println("Add:      " + SigFig.add(x,y));
-        //     if(a > b) {
-        //         System.out.println("Subtract: " + SigFig.subtract(x, y));
-        //     } else {
-        //         System.out.println("Subtract: " + SigFig.subtract(y, x));
-        //     }
-        //     System.out.println("Multiply: " + SigFig.multiply(x, y));
-        //     System.out.println("Divide:   " + SigFig.divide(x,y));
-        //     System.out.println();
-        // }
-        System.out.println(SigFig.subtract("0.05", "0.02"));
+        // Scanner sc = new Scanner(System.in);
+        // String a1 = sc.nextLine();
+        // String b1 = sc.nextLine();
+        // String a2 = sc.nextLine();
+        // String b2 = sc.nextLine();
+        // System.out.println(Cross.multiply(a1, b1, a2, b2));
+        // System.out.println(Cross.divide(a1, b1, a2, b2));
+        System.out.println(Integer.parseInt("+02"));
+        System.out.println(SigFig.set(0.00000000012345, 2));
     }
 }
